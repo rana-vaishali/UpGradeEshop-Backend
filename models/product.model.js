90b484b4f0d4682fb9b335c21630296ef3a7f4ea
vaishali <51531318+rana-vaishali@users.noreply.github.com>
2022-07-22 22:07:34 +0530

    API Endpoint3

--- models/product.model.js
+++ models/product.model.js
@@ -1,20 +1,59 @@
-module.exports = mongoose => {
-    const Product =mongoose.model(
-                "product",
-        mongoose.Schema(
-            {
-                name:{type:String},
-                availableItems:{type:Number},
-                category:{type:String},
-                createdAt:{type:Date},
-                description:{type:String},
-                imageURL:{type:String},
-                manufacturer:{type:String},
-                price:{type:Number},
-                updatedAt:{type:Date}
+const Joi = require("joi");
+const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 
-            }
-        )
-    );
-    return Product;
-};
+const productSchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  manufacturer: String,
+  availableItems: Number,
+  price: Number,
+  imageURL: {
+    type: String,
+  },
+  description: String,
+  createdAt :{
+    type:Date,
+    default: Date.now()
+  },
+  updatedAt:{
+    type:Date,
+    default: Date.now()
+  }
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+function validateProduct(product) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+    availableItems: Joi.number().positive().required(),
+    price: Joi.number().positive().required(),
+    category: Joi.string().min(3).required(),
+    description: Joi.string().min(3).required(),
+    imageURL: Joi.string().min(3).required(),
+    manufacturer: Joi.string().min(3).required(),
+    createdAt: Joi.date(),
+    updatedAt: Joi.date()
+  };
+  return Joi.validate(product, schema); // change the type
+}
+
+function validateProductGet(product) {
+  const schema = {
+    name: Joi.string().default(""),
+    category: Joi.string().default(""),
+    direction: Joi.any().valid('ASC','DESC').default('DESC'),
+    sortBy: Joi.string().default('productId'),
+  };
+  return Joi.validate(product, schema); // change the type
+}
+
+exports.Product = Product; 
+exports.validateProduct = validateProduct;
+exports.validateProductGet = validateProductGet;
+exports.productSchema = productSchema;
+exports.Product = Product;
+exports.validateProduct = validateProduct;
+exports.validateProductGet = validateProductGet;
